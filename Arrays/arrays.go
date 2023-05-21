eebe6d1576e93a8daccc07ec75c680ee57395648
leen-neel <and24903@gmail.com>
2023-05-21 21:40:41 +0530

    arrays

---
+++ Arrays/arrays.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nums [3]int
+
+	nums[0] = 1
+	nums[1] = 2
+	nums[2] = 3
+
+	fmt.Println(nums)
+
+}
--- conditionals/ifelse.go
+++ conditionals/ifelse.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+
 	a := 4
 
 	if a == 2 {
