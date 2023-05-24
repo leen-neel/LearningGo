d6a8121697b9acbf55d6be0134d31f7a38c1a837
leen-neel <and24903@gmail.com>
2023-05-24 20:37:15 +0530

    table

--- Loops/loops.go
+++ Loops/loops.go
@@ -4,20 +4,13 @@ import "fmt"
 
 func main() {
 
-	sum := 0
+	var num int
 
-	for i := 0; i < 10; i++ {
-		sum += i
-		fmt.Println(i)
-	}
+	fmt.Print("Enter a number : ")
+	fmt.Scan(&num)
 
-	fmt.Printf("\n%T \n", sum)
-	fmt.Println(sum)
-
-	arr := []int{1, 2, 3}
-
-	for _, value := range arr {
-		fmt.Printf("%d ", value)
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("%d * %d = %d\n", num, i, num*i)
 	}
 
 }
